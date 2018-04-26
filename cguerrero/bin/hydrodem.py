ad97faa7d02d774286ace70dfea1caa87223d6d7
cguerrerocordova <cguerrerocordova@gmail.com>
2018-04-26 00:24:23 -0300

    Running from bin.

---
+++ cguerrero/bin/hydrodem.py
@@ -0,0 +1,11 @@
+"""
+HydroDEM
+"""
+from cguerrero.hydrodem.HydroDEMProcess import HydroDEMProcess
+
+__author__ = "Cristian Guerrero"
+__copyright__ = "Copyright 2018"
+
+if __name__ == '__main__':
+    hydro_dem = HydroDEMProcess()
+    hydro_dem.start()
